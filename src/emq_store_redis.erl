a6dec438d15e645c1a498d945dc4daa0b22187e6
zhangyao@jcble.com <zhangyao@jcble.com>
2018-09-26 11:49:19 +0800

    消息持久化单进程版

--- src/emq_store_redis.erl
+++ src/emq_store_redis.erl
@@ -38,9 +38,9 @@
 -export([on_session_subscribed/4, on_message_publish/2, on_message_acked/4]).
 
 %% 
--export([read_messages/3, match_messages/3]).
+-export([read_messages/4, match_messages/4]).
 
--record(state, {redis_client, send_interval, send_timer, host, port, db, password}).
+-record(state, {redis_client, read_interval, host, port, db, password}).
 
 %% Called when the plugin application start
 load(Env) ->
@@ -51,21 +51,19 @@ load(Env) ->
 %% 启动一个线程来处理持久化消息的发生，完毕后线程自动退出
 on_session_subscribed(_ClientId, _Username, {Topic, _Opts}, _Env) ->
     Time = erlang:timestamp(),
-    io:format("extract for ~p~n", [Topic]),
-    extract_messages(Topic, Time).
+    extract_messages(Topic, timestamp_to_system_time(Time)).
 
 %% transform message and return
 on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env) ->
     {ok, Message};
 
-on_message_publish(Message = #mqtt_message{topic = Topic}, _Env) ->
-    io:format("publish ~s~n", [emqttd_message:format(Message)]),
-    %%subs = lists:append(emqttd_router:match(Topic),emqttd_router:match_local(Topic)),
+on_message_publish(Message, _Env) ->
+    lager:debug("on publish ~s~n", [emqttd_message:format(Message)]),
     store_message(Message),
     {ok, Message}.
 
 on_message_acked(ClientId, Username, Message, _Env) ->
-    io:format("client(~s/~s) acked: ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
+    lager:debug("client(~s/~s) acked: ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
     delete_message(Message),
     {ok, Message}.
 
@@ -96,25 +94,22 @@ start_link(Env) ->
 %% gen_server Callbacks
 %%--------------------------------------------------------------------
 
+%% 启动redis进程池来处理消息的存储
 init([Env]) ->
-    io:format("env: ~p~n", [Env]),
     Host = proplists:get_value(host, Env, 'localhost'),
     Port = proplists:get_value(port, Env, 6379),
     Db = proplists:get_value(database, Env, 0),
     Password = proplists:get_value(password, Env, []),
     Retry = proplists:get_value(retry, Env, 1000),
-    io:format("redis: ~p ~p ~p ~p ~p~n", [Host, Port, Db, Password, Retry]),
+    lager:info("redis info: ~p ~p ~p ~p~n", [Host, Port, Db, Retry]),
     {ok, C} = eredis:start_link(Host, Port, Db, Password, Retry),
-    State = #state{redis_client = C, host = Host, port = Port, db = Db, password = Password},
-    {ok, start_send_timer(proplists:get_value(send_interval, Env, 0), State)}.
-
-start_send_timer(0, State) ->
-    State#state{send_interval = 0, send_timer = undefined};
-start_send_timer(undefined, State) ->
-    State#state{send_interval = 0, send_timer = undefined};
-start_send_timer(Ms, State) ->
-    {ok, Timer} = timer:send_interval(Ms, send),
-    State#state{send_interval = Ms, send_timer = Timer}.
+    State = #state{redis_client = C, 
+                   read_interval = proplists:get_value(read_interval, Env, 0), 
+                   host = Host, 
+                   port = Port, 
+                   db = Db, 
+                   password = Password},
+    {ok, State}.
 
 handle_call({extract, Topic, Deadline}, _From, State) ->
     extract(Topic, Deadline, State),    
@@ -131,20 +126,10 @@ handle_call(Req, _From, State) ->
 handle_cast(Msg, State) ->
     ?UNEXPECTED_MSG(Msg, State).
 
-handle_info(send, State = #state{send_interval = Never})
-    when Never =:= 0 orelse Never =:= undefined ->
-    {noreply, State, hibernate};
-
-handle_info(send, State = #state{send_interval = Interval}) ->
-    %% 查询未应答消息，并publish
-
-    {noreply, State, hibernate};
-
 handle_info(Info, State) ->
     ?UNEXPECTED_INFO(Info, State).
 
-terminate(_Reason, _State = #state{redis_client = C, send_timer = TRef1}) ->
-    timer:cancel(TRef1),
+terminate(_Reason, _State = #state{redis_client = C}) ->
     eredis:stop(C).
 
 code_change(_OldVsn, State, _Extra) ->
@@ -154,76 +139,101 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Functions
 %%--------------------------------------------------------------------
 
-extract(Topic, Deadline, State = #state{host = Host, port = Port, db = Db, password = Password}) ->
+extract(Topic, Deadline, #state{read_interval = Read_interval, host = Host, port = Port, db = Db, password = Password}) ->
     case emqttd_topic:wildcard(Topic) of
-        false -> spawn(?MODULE, read_messages, [{Host, Port, Db, Password}, Topic, Deadline]);
-        true  -> spawn(?MODULE, match_messages, [{Host, Port, Db, Password}, Topic, Deadline])
+        false -> spawn(?MODULE, read_messages, [{Host, Port, Db, Password}, Topic, Deadline, Read_interval]);
+        true  -> spawn(?MODULE, match_messages, [{Host, Port, Db, Password}, Topic, Deadline, Read_interval])
     end.
 
-read_messages({Host, Port, Db, Password}, Topic, Deadline) ->
+read_messages({Host, Port, Db, Password}, Topic, Deadline, Read_interval) ->
     {ok, Client} = eredis:start_link(Host, Port, Db, Password, 1000),
-    scan_redis(Client, <<"0">>, make_pattern(Topic), 10, Deadline, read_send/3),
+    scan_sets(Client, Topic, 0, Deadline, 10, Read_interval),
     eredis:stop(Client).
 
-match_messages({Host, Port, Db, Password}, Topic, Deadline) ->
+match_messages({_Host, _Port, _Db, _Password}, _Topic, _Deadline, _Read_interval) ->
     ignore.
 
-read_send(_Client, [], _) ->
-    ignore;
-read_send(Client, Keys, Deadline) ->
-    lists:foreach(fun(Key) ->
-        case eredis:q(Client, ["GET", Key]) of
-            {ok, undefined} ->
-                ignore;
-            {ok, Bin} ->
-                Message = binary_to_term(Bin),
-                publish(Message);
-                %case binary_to_term(Bin) of
-                %    Message = #mqtt_message{topic = Topic, timestamp = Time} when -> 
-                
-                
-            {_, _} ->
-                ignore
-        end end, Keys).
-
-publish(Message = #mqtt_message{topic = Topic}) ->
-    emqttd_pubsub:publish(Topic, Message), ok.
+publish(Message = #mqtt_message{topic = Topic, timestamp = Timestamp}) ->
+    emqttd_pubsub:publish(Topic, Message), 
+    timestamp_to_system_time(Timestamp).
     
-
-scan_redis(C, Start, Pattern, Count, Deadline, Fun) ->
-    case eredis:q(C, ["SCAN", Start, "MATCH", Pattern, "COUNT", Count]) of
-        {ok, [<<"0">>, Keys]} ->
-            Fun(C, Keys, Deadline);
-        {ok, [Cursor, Keys]} ->
-            Fun(C, Keys, Deadline),
-            scan_redis(C, Cursor, Pattern, Count, Deadline, Fun)
+%%
+%% 根据scope对SortedSet进行遍历，起始值为0，截止值为当前时间（微秒）
+%% LIMIT只限制每次查询的条数，若使用LIMIT来遍历，会出现有些消息无法被遍历到。
+%% 原因是遍历后会publish，客户端应答后记录会被删除，导致LIMIT的索引失效。
+%%
+scan_sets(C, Key, Start, Deadline, Count, Interval) ->
+    case eredis:q(C, ["ZRANGEBYSCORE", Key, to_scope(Start), integer_to_binary(Deadline), "LIMIT", 0, Count]) of
+        {ok, []} ->
+            ok;
+        {ok, Members} ->
+            Upper = 
+            lists:foldl(fun(M, Acc) -> 
+                                case eredis:q(C, ["GET", M]) of
+                                    {ok, undefined} ->
+                                        io:format("scan ignore~n"),
+                                        Acc;
+                                    {ok, Bin} ->
+                                        Message = binary_to_term(Bin),
+                                        publish(Message);
+                                    {_, _} ->
+                                        io:format("scan ignore~n"),
+                                        Acc end end, 0, Members),
+            timer:sleep(Interval),
+            scan_sets(C, Key, Upper, Deadline, Count, Interval)
     end.
 
-make_pattern(Topic) ->
-    Space = <<":">>,
-    P = <<"*">>,
-    <<Topic/binary, Space/binary, P/binary>>.   
+to_scope(Num) ->
+    Bracket = <<"(">>,
+    N = integer_to_binary(Num),
+    <<Bracket/binary, N/binary>>.
 
-make_full_key(Topic, MsgId) ->
-    Space = <<":">>,
-    <<Topic/binary, Space/binary, MsgId/binary>>.
-
-store(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _State) ->
+%% 存储消息使用SortedSet+Hash，SortedSet用户消息排序，Hash存储具体的消息。
+%% SortedSet和Hash之间使用mqtt_message.id关联(pktid，存储和应答的值不一致)
+store(#mqtt_message{topic = <<"$SYS/", _/binary>>}, _State) ->
     ok;
-store(Message = #mqtt_message{topic = Topic, id = MsgId}, State = #state{redis_client = Client}) ->
-    io:format("save: msgId ~p -> ~p~n", [MsgId, Topic]),
-    Key = make_full_key(Topic, MsgId),
-    {ok, <<"OK">>} = eredis:q(Client, ["SET", Key, term_to_binary(Message)]),
+store(Message = #mqtt_message{topic = Topic, timestamp = Timestamp, id = MsgId}, #state{redis_client = Client}) ->
+    save_mix(Client, Topic, timestamp_to_system_time(Timestamp), MsgId, term_to_binary(Message)),
     ok.
 
-delete(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _State) ->
+save_kv(Client, Key, Value) ->
+    {ok, <<"OK">>} = eredis:q(Client, ["SET", Key, Value]).
+
+%% 使用Sorted Set和Hash实现排序和存储
+save_mix(Client, Key, Scope, Member, Value) ->
+    case eredis:q(Client, ["ZADD", Key, Scope, Member]) of
+        {ok, <<"1">>} ->
+            save_kv(Client, Member, Value);
+        _ ->
+            ignore
+    end.
+
+
+delete(#mqtt_message{topic = <<"$SYS/", _/binary>>}, _State) ->
     ok;
-delete(Message = #mqtt_message{topic = Topic, id = MsgId}, State = #state{redis_client = Client}) ->
-    io:format("delete: msgId ~p -> ~p~n", [MsgId, Topic]),
-    Key = make_full_key(Topic, MsgId),
+delete(#mqtt_message{topic = Topic, id = MsgId}, #state{redis_client = Client}) ->
+    remove_mix(Client, Topic, MsgId).
+
+remove_kv(Client, Key) ->
     case eredis:q(Client, ["DEL", Key]) of
         {ok, _} ->
             ok;
         _ ->
             ignore
     end.
+remove_mix(Client, Key, Member) ->
+    case eredis:q(Client, ["ZREM", Key, Member]) of
+        {ok, <<"1">>} ->
+            %io:format("delete ok~n"),
+            remove_kv(Client, Member),
+            ok;
+        {ok, <<"0">>} ->
+            %io:format("delete failed no such Member~n"),
+            ignore;
+        _ ->
+            ignore
+    end.
+
+timestamp_to_system_time({MegaSecs, Secs, MicroSecs}) ->
+    MegaSecs * 1000000000000 + Secs * 1000000 + MicroSecs.
+
